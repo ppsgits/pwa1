112623a842c9d238f0d6dcaff05e97d2c6dbe142
Purslane Gill <praveengill@iitbombay.org>
2024-11-10 05:06:37 +0530

    Add QR Cde Scanner PWA

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// service-worker.js
+const CACHE_NAME = 'qr-scanner-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon.png',
+    'https://unpkg.com/html5-qrcode/minified/html5-qrcode.min.js'
+];
+
+// Install event: cache files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event: serve assets from cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
